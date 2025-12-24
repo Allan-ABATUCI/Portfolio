c0d7c5b03d076862d4838b3e861cf3a88872a633
Allan-ABATUCI <allanabatucipro@gmail.com>
2025-12-24 18:10:24 +0100

    des et header front ok

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,6 @@ export default function RootLayout({
         <div className="bg-[#dbd7fb] absolute -top-4 -z-10 -left-140 h-125 w-200 rounded-full blur-[10rem] 
         sm:w-275 md:-left-132 lg:-left-112 xl:-left-60 2xl:-left-20 dark:bg-[#676394]"></div>
         <Header/>
-
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import Description from "@/components/Description";
 import Image from "next/image";
-
+import SectionDivider from "@/components/SectionDivider";
 export default function Home() {
   return (
-   <></>
+   <main className="flex-col items-center text-center">
+    <Description/>
+    <SectionDivider/>
+   </main>
   );
 }
