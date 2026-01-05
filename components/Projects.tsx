466de1b49927c6ce552561c26a71e98c1b189ada
Allan-ABATUCI <allanabatucipro@gmail.com>
2026-01-05 22:16:21 +0100

    eh

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -10,23 +10,25 @@ export default function Projects(){
                     Mes Projets
                 </h2>
 
-                {projectsData.map((Projet)=>(
-                    <section className="bg-gray-100 max-w-2xl border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-80 hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
-                        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-72">
+                {projectsData.map((Projet,index)=>(
+                    <section key={index} className=" bg-gray-100 max-w-2xl border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-80 hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
+                        <section className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-72">
                             <h3 className="text-2xl font-semibold">{Projet.title}</h3>
                             <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
-                                {Projet.title}
+                                {Projet.description}
                             </p>
+                        
                             <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
-                                {Projet.tags.map((tag) => (
+                                {Projet.tags.map((tag,index) => (
                                 <li
+                                key={index}
                                     className="bg-black/70 px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70"
                                 >
                                     {tag}
                                 </li>
                                 ))}
                             </ul>
-                        </div>
+                        </section>
                     </section>
                 ))}
             </motion.div>
